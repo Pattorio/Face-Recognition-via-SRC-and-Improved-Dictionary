af8297fbd3e6be9346b754e9b97e5ee0a3f99eb2
Yimeng Zhou <yz2993@columbia.edu>
2017-05-03 19:47:29 -0400

    update all

--- inexact_alm_lrr.m
+++ inexact_alm_lrr.m
@@ -11,9 +11,13 @@ mu = 10e-6;
 mu_max = 10e6;
 rho = 1.1;
 tol = 10e-8;
+maxIter = 1e6;
 
 [d,n] = size(X);
 m = size(A,2);
+
+atx = A'*X;
+inv_a = inv(A'*A + eye(m));
 
 % initialize
 J = zeros(m,n);
@@ -31,8 +35,66 @@ while ~converged
     % update J
     temp_J = Z + Y2/mu;
     [U,S,V] = svd(temp,'econ');
-    S_diag = diag(S);
+    
+    sigma = diag(S);
     svp = lenth(find(sigma>1/mu));
+    if svp >= 1
+        sigma = sigma(1:svp) - 1/mu;
+    else
+        svp = 1;
+        sigma = 0;
+    end
+    J = U(:,1:svp)*diag(sigma)*V(:,1:svp)';
+    
+    % update Z
+    Z = inv_a * (atx - A'*E + J + (A'*Y1 - Y2)/mu);
+    
+    % update E
+    xmaz = X - A*Z;
+    Q = xmaz+Y1/mu;
+    alpha = lambda/mu;
+    for i = 1:n
+        Q_norm = norm(Q(:,i),2);
+        if  Q_norm > alpha
+            E(:,i) = (Q_norm - alpha)/Q_norm * Q(:,i);
+        else
+            E(:,i) = zero(n,1);
+        end
+    end
+    
+    % update Y1
+    xmazme = xmaz - E;
+    Y1 = Y1 + mu*xmazme;
+    
+    % update Y2
+    zmj
+    Y2 = Y2 - mu*zmj;
+    
+    % update mu
+    mu = min(rho*mu, u_max);
+    
+    condition = max(max(max(abs(xmazme))),max(max(abs(zmj))));
+    
+    if (mod(iter,50) == 0) || (condition < tol)
+        disp('The number of iteration = ' num2str(iter));
+    end
+    
+    if iter == maxIter && condtition > tol
+        converged = 1;
+        disp('Reach max iteration');
+    end
+    
+    if condition < tol
+        converged = 1;
+    end
+end
+
+    
+    
+    
+    
+    
+        
     
 
 
