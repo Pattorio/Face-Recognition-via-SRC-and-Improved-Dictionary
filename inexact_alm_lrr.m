828fd07cd7335494a523872c88f3c5218f8e0768
Yimeng Zhou <yz2993@columbia.edu>
2017-05-04 18:32:19 -0400

    update lrr

--- inexact_alm_lrr.m
+++ inexact_alm_lrr.m
@@ -1,102 +1,73 @@
 function [Z,E] = inexact_alm_lrr(X,A,lambda)
-% min |Z|_* + lamnda * |E|_2,1  s.t. X = AZ+E;
-% X - d x n matrix
-% A - d x m matrix
-% Z - m x n matrix
-% E - d x n matrix
-% tol is the tolerance of convergence conditions
-% maxIter is the maximum number of iterations
+% This routine uses Inexact ALM algorithm to solve the following nuclear-norm optimization problem:
+% min |Z|_*+lambda*|E|_2,1
+% s.t., X = AZ+E
+% inputs:
+%        X -- D*N data matrix, D is the data dimension, and N is the number
+%             of data vectors.
+%        A -- D*M matrix of a dictionary, M is the size of the dictionary
 
-mu = 10e-6;
-mu_max = 10e6;
-rho = 1.1;
-tol = 10e-8;
+%% initialization
+tol = 1e-8;
 maxIter = 1e6;
-
-[d,n] = size(X);
+[d n] = size(X);
 m = size(A,2);
-
+rho = 1.1;
+max_mu = 1e10;
+mu = 1e-6;
 atx = A'*X;
-inv_a = inv(A'*A + eye(m));
+inv_a = inv(A'*A+eye(m));
 
-% initialize
+%% Initializing optimization variables
 J = zeros(m,n);
 Z = zeros(m,n);
 E = sparse(d,n);
+
 Y1 = zeros(d,n);
 Y2 = zeros(m,n);
 
+%% Start main loop
 iter = 0;
-converged = false;
 
-while ~converged
+while iter<maxIter
     iter = iter + 1;
+    if mod(iter,50) == 0
+        disp(['The number of iteration = ' num2str(iter)]);
+    end
+    %update J
+    temp = Z + Y2/mu;
     
-    % update J
-    temp_J = Z + Y2/mu;
-    [U,S,V] = svd(temp,'econ');
+    [U,S,V] = svd(temp, 'econ'); % stable 
+    %[U,S,V] = svdecon(temp); % fastest, but EIG must not contain NaN or Inf.
     
     sigma = diag(S);
-    svp = lenth(find(sigma>1/mu));
-    if svp >= 1
-        sigma = sigma(1:svp) - 1/mu;
+    svp = length(find(sigma>1/mu));
+    if svp>=1
+        sigma = sigma(1:svp)-1/mu;
     else
         svp = 1;
         sigma = 0;
     end
     J = U(:,1:svp)*diag(sigma)*V(:,1:svp)';
+    %udpate Z
+    Z = inv_a*(atx-A'*E+J+(A'*Y1-Y2)/mu);
+    %update E
+    xmaz = X-A*Z;
+    temp = xmaz+Y1/mu;
+    E = solve_l1l2(temp,lambda/mu);
     
-    % update Z
-    Z = inv_a * (atx - A'*E + J + (A'*Y1 - Y2)/mu);
-    
-    % update E
-    xmaz = X - A*Z;
-    Q = xmaz+Y1/mu;
-    alpha = lambda/mu;
-    for i = 1:n
-        Q_norm = norm(Q(:,i),2);
-        if  Q_norm > alpha
-            E(:,i) = (Q_norm - alpha)/Q_norm * Q(:,i);
-        else
-            E(:,i) = zero(n,1);
-        end
+    leq1 = xmaz-E;
+    leq2 = Z-J;
+    stopC = max(max(max(abs(leq1))),max(max(abs(leq2))));
+    if (iter==1 || mod(iter,50)==0 || stopC<tol)
+        disp(['iter ' num2str(iter) ',mu=' num2str(mu,'%2.1e') ...
+            ',rank=' num2str(rank(Z,1e-3*norm(Z,2))) ',stopALM=' num2str(stopC,'%2.3e')]);
     end
-    
-    % update Y1
-    xmazme = xmaz - E;
-    Y1 = Y1 + mu*xmazme;
-    
-    % update Y2
-    zmj
-    Y2 = Y2 - mu*zmj;
-    
-    % update mu
-    mu = min(rho*mu, u_max);
-    
-    condition = max(max(max(abs(xmazme))),max(max(abs(zmj))));
-    
-    if (mod(iter,50) == 0) || (condition < tol)
-        disp(['The number of iteration = ' num2str(iter)]);
-    end
-    
-    if iter == maxIter && condtition > tol
-        converged = 1;
-        disp('Reach max iteration');
-    end
-    
-    if condition < tol
-        converged = 1;
+    if stopC<tol 
+        break;
+    else
+        Y1 = Y1 + mu*leq1;
+        Y2 = Y2 + mu*leq2;
+        mu = min(max_mu,mu*rho);
     end
 end
-
-    
-    
-    
-    
-    
-        
-    
-
-
-
-
