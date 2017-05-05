3757f0ee7f7c5f465b6663331548ae38665ed844
Qianwen Zheng <zhengqw77@gmail.com>
2017-05-05 15:39:23 -0400

    update function

--- getDelta.m
+++ getDelta.m
@@ -1,9 +1,9 @@
-function [delta_maxNorm, delta_matrix]= getDelta(x_hat,K)
+function [delta_maxNorm, delta_matrix]= getDelta(x_hat,K,sample_num)
 X = repmat(x_hat,1,K);
 delta_matrix = zeros(size(X));
 for k=1:K
-    i_start = (k-1)*19+1;
-    i_end = k*19;
+    i_start = (k-1)*sample_num+1;
+    i_end = k*sample_num;
     delta_matrix(i_start:i_end,k) = X(i_start:i_end,k);
     norm1_delta(k) = norm(delta_matrix(:,k),1);
 end
--- isValid.m
+++ isValid.m
@@ -1,15 +1,15 @@
-function [isvalid,delta_x,SCI] = isValid(x_hat,K)
+function [isvalid,delta_x,SCI] = isValid(x_hat,K,sample_num)
 % x_hat - n x 1 vector
 % K - the total number of in-set phase classes
 % m = K*n --- m = size(A,2)
 %         --- n - the number of training tokens per class
 % tau - threshold 
     norm1_x = norm(x_hat,1);
-    [delta_x,M] = getDelta(x_hat,K);
+    [delta_x,M] = getDelta(x_hat,K,sample_num);
     
     SCI = (K*(delta_x/norm1_x)-1)/(K-1);
     
-    tau = 0.2;
+    tau = 0.15;
     if SCI>=tau
         isvalid = true;
     else
