be613fb80b4cbbd63fe9bbad3f62da16134e9f51
Yimeng Zhou <yz2993@columbia.edu>
2017-05-05 20:30:48 -0400

    update

--- main.m
+++ main.m
@@ -5,37 +5,42 @@ disp('Load imaged successfully!');
 downsample_size = img_size./3;
 
 trA = im2double(trA);
-%temp_te_index = sort([2,30,55,77]);%1,5,10,13
-%tempTeA = teA(:,temp_te_index);
-%te_index = te_index(:,temp_te_index);
-%teA = im2double(tempTeA);
 teA = im2double(teA);
 
 num_tr = size(trA, 2);
 num_te = size(teA, 2);
 
+%{
 % normalize dictionanry 
 for i = 1:num_tr
     trA(:,i) = trA(:,i)/norm(trA(:,i),2);
 end
+for i = 1:num_te
+    teA(:,i) = teA(:,i)/norm(teA(:,i),2);
+end
+%}
 
 % RPCA
 % min |A|_* + lambda|E|_1 s.t D = A + E 
 % Recovery a low-rank matrix A from a corrupted data matrix D
 % [A,E] = inexact_alm_rpca(D,lambda)
+
 disp('Do RPCA for dictionary...');
 lambda_rpca = 1/sqrt(size(trA, 1));
+
 [A_rpca,E_rpca] = inexact_alm_rpca(trA,lambda_rpca);
 disp('Finish RPCA!');
 
 %{
 for i = 1:num_tr
-    temp_img = reshape(A_rpca(:,i),downsample_size);
+    temp_img = reshape(E_rpca(:,i),downsample_size);
     image8Bit = uint8(255 * mat2gray(temp_img));
     imshow(image8Bit);
 end
 %}
 
+
+%{
 % LRR
 % min rank(Z) + lambda|E|_2,1 s.t X = AZ + E 
 % where Z is the low rank representataion of X
@@ -45,54 +50,45 @@ end
 disp('Do LRR for dictionary...');
 
 lambda_lrr = 1/sqrt(size(trA, 1));
-[Z_lrr,E_lrr] = solve_lrr(trA,lambda_lrr);
+[Z_lrr,E_lrr] = LRR(trA,lambda_lrr);
 A_lrr = trA * Z_lrr;
 disp('Finish LRR!');
+%}
 
 % SRC intitialization
 m_rpca = size(A_rpca,1);
-m_lrr = size(A_lrr,1);
-m_raw = size(trA,1);
+m = size(trA,1);
 
 % B = [A,I];
 B_rpca = [A_rpca,eye(m_rpca)];
-B_lrr = [A_lrr,eye(m_lrr)];
-B_raw = [trA,eye(m_raw)];
+B = [trA, eye(m)];
+
 
 rho = 1.5;
-alpha_rpca = 1/sqrt(size(B_rpca, 1));
-alpha_lrr = 1/sqrt(size(B_lrr, 1));
-alpha_raw = 1/sqrt(size(B_raw,1));
+alpha = lambda_rpca;
 
 % ADMM for dictionary from RPCA
 for i = 1:num_te
-    [w_rpca(:,i), history] = basis_pursuit(B_rpca, teA(:,i), rho, alpha_rpca);
-end
-
-% ADMM for dictionary from LRR
-for i = 1:num_te
-    [w_lrr(:,i), history] = basis_pursuit(B_lrr, teA(:,i), rho, alpha_lrr);
+    [w_rpca(:,i), history_rpca] = basis_pursuit(B_rpca, teA(:,i), rho, alpha);
 end
 
 % ADMM for raw dictionary
 for i = 1:num_te
-    [w_raw(:,i), history] = basis_pursuit(B_raw, teA(:,i), rho, alpha_raw);
+    [w(:,i),history] = basis_pursuit(B,teA(:,i),rho,alpha);
 end
 
+
 % w = [x;e]
+x = w(1:num_tr,:);
+e = w((num_tr+1):end,:);
+
 x_rpca = w_rpca(1:num_tr,:);
 e_rpca = w_rpca((num_tr+1):end,:);
 
-x_lrr = w_lrr(1:num_tr,:);
-e_lrr = w_lrr((num_tr+1):end,:);
-
-x_raw = w_raw(1:num_tr,:);
-e_raw = w_raw((num_tr+1):end,:);
-
-% recovery 
+% recovery
 Y_rpca = A_rpca * x_rpca;
-Y_lrr = A_lrr * x_lrr;
-Y_raw = trA * x_raw;
+Y = trA * x;
+
 
 
 %{
